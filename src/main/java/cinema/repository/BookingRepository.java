c9edee52811b3a4685918ce915f86faa1b9902f1
shakira <147226750+strongerwo@users.noreply.github.com>
2026-05-11 09:33:50 +0700

    flutter setted

--- src/main/java/cinema/repository/BookingRepository.java
+++ src/main/java/cinema/repository/BookingRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
-    List<Booking> findByUserId(int userId);
+    List<Booking> findByUser_UserId(int userId);
 }
