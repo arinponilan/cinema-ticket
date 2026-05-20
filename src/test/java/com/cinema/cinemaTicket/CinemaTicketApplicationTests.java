38453b317bea59587614f82a696a92de1d5f87a2
tianisasianipar <tianisasianipar@gmail.com>
2026-05-20 21:53:32 +0700

    backup local cinema-ticket

---
+++ src/test/java/com/cinema/cinemaTicket/CinemaTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.cinemaTicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
